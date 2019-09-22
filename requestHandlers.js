5701955d3b65b899a8481bb966a463365a8c99ec
Pat Haynes <pathaynes32@gmail.com>
2019-09-22 16:23:53 -0700

    pretty much done

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,3 +1,7 @@
+const querystring = require('querystring'),
+  fs = require('fs'),
+  formidable = require('formidable');
+
 function start(response) {
   console.log('request handler start was called.');
 
@@ -7,9 +11,10 @@ function start(response) {
     'content="text/html; charset=UTF-8" />' +
     '</head>' +
     '<body>' +
-    '<form action="/upload" method="post">' +
-    '<textarea name="text" rows="20" cols="60"></textarea>' +
-    '<input type="submit" value="Submit text" />' +
+    '<form action="/upload" enctype="multipart/form-data">' +
+    'method="post">' +
+    'input type="file" name="upload">' +
+    '<input type="submit" value="Upload file" />' +
     '</form>' +
     '</body>' +
     '</html>';
@@ -19,12 +24,29 @@ function start(response) {
   response.end();
 }
 
-function upload(response) {
+function upload(response, request) {
   console.log('request handler upload was called.');
+
+  const form = new formidable.IncomingForm();
+  console.log('about to parse');
+  form.parse(request, function(error, fields, files) {
+    if(error) {
+      fs.unlink('tmp/bicycle.png');
+      fs.rename(files.upload.path, 'tmp/bicycle.png');
+    }
+  });
   response.writeHead(200, { 'Content-Type': 'text/plain' });
-  response.write('Hello Upload');
+  response.write('received image:<br/>');
+  response.write(`<img src='/show' />`);
   response.end();
+}
+
+function show(response) {
+  console.log('request handler show was called.');
+  response.writeHead(200, { 'Content-type': 'image/png' });
+  fs.createReadStream('tmp/bicycle.png').pipe(response);
 }
 
 exports.start = start;
 exports.upload = upload;
+exports.show = show;
--- server.js
+++ server.js
@@ -5,11 +5,11 @@ function start(route, handle) {
   function onRequest(request, response) {
     const pathname = url.parse(request.url).pathname;
     console.log(`request for ${pathname} received.`);
-
-    route(handle, pathname, response);
+    route(handle, pathname, response, request);
   }
   http.createServer(onRequest).listen(8888);
-  console.log('server has started');
+  console.log('server has started.');
+  
 }
 
 exports.start = start;
