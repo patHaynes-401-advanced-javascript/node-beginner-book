7b274e765b35de7959abe240e8517642d762b650
Pat Haynes <pathaynes32@gmail.com>
2019-09-21 21:35:17 -0700

    added router

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
-var http = require('http');
+const http = require('http');
+const url = require('url');
 
-http.createServer(function(request, response) {
-  response.writeHead(200, { 'Content-Type': 'text/plain' });
-  response.write('Hello World');
-  response.end();
-}).listen(8888);
+function start(route) {
+  function onRequest(request, response) {
+    const pathname = url.parse(request.url).pathname;
+    console.log(`request for ${pathname} received.`);
+
+    route(pathname);
+
+    response.writeHead(200, { 'Content-Type': 'text/plain' });
+    response.write('Yo this shit is fucking working! Lets party!');
+    response.end();
+  }
+  http.createServer(onRequest).listen(8888);
+  console.log('server has started');
+}
+
+exports.start = start;
